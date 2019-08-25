169b6962db90131afee77fc957f91d337809d4f0
Andy Durette <duretteandy@gmail.com>
2019-08-25 17:20:13 -0400

    Update setting a mobile menu

--- src/index.js
+++ src/index.js
@@ -12,28 +12,34 @@ class App extends React.Component {
       weather: {},
       isFetching: true,
       isOpenValue: 0,
-      isActive: 0
+      isActive: 0,
+      menuActive: false
     }
+
+    // Actual Url: https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7e2a14caaf233206e9baa745d4d549c1/43.6532,-79.3832
+    // Testing Url: https://cors-anywhere.herokuapp.com/http://andydurette.com/sitelog/test-data/weather.json
 
     fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/7e2a14caaf233206e9baa745d4d549c1/43.6532,-79.3832')
      .then((res) => res.json())
      .then((data) => this.setState({weather: data}))
      .then(() => this.setState({ isFetching: false}))
-     .then(() => {} )
   }
 
+  // Function that calls date information to display with weather Api Data
   dateset = (i) =>{
     var date = new Date();
     date.setDate(date.getDate() + i);
     return date.toString().substring(0,3);
     }
 
+  // Function that returns a formated date string with the key number represented by i for the day 
   datesetFull = (i) =>{
     var date = new Date();
     date.setDate(date.getDate() + i);
     return date.toString().slice(0,15) + ' ' +  date.toString().substring(33);
     }
   
+  // Compares the api text for weather to use an image, tried using a switch operator instead of if but had errors
   weatherIcon = (i) => {
     const icon = this.state.weather.daily.data[i].icon;
 
@@ -60,16 +66,28 @@ class App extends React.Component {
     }
   }
 
+  // Takes temperature from api data based off the day using i and converts it for Canadian reporting.
   tempHigh = (i) => {
     return Math.round((this.state.weather.daily.data[i].temperatureHigh-32)/1.8);
   }
 
+  // Takes temperature from api data based off the day using i and converts it for Canadian reporting.
   tempLow = (i) => {
     return Math.round((this.state.weather.daily.data[i].temperatureLow-32)/1.8);
   }
 
+  // Takes summary from api data based off the day using i and reports.
   summary = (i) => {
     return this.state.weather.daily.data[i].summary;
+  }
+
+  // Changes state to update weather mobile menu should appear
+  menuUpdate = () => {
+    if (this.state.menuActive === false ){
+      this.setState({ menuActive: true})
+    }else{
+      this.setState({ menuActive: false});
+    }
   }
 
   render() {
@@ -82,6 +100,7 @@ class App extends React.Component {
           : (
             <React.Fragment>
             <h1>Toronto Weekly Weather Forecast</h1>
+            {/* Displays main content, the larger section about weather based on the day*/}
             <WeatherDisplay
               day={this.datesetFull(this.state.isOpenValue)}
               weatherIcon={this.weatherIcon(this.state.isOpenValue)}
@@ -89,7 +108,9 @@ class App extends React.Component {
               tempLow={this.tempLow(this.state.isOpenValue)} 
               summary={this.summary(this.state.isOpenValue)}
             ></WeatherDisplay>
-            <section id='weatherContainer'>
+
+            {/* Controls for weather display */}
+            <section id='weatherContainer' className={(this.state.menuActive === false ) ? "close" : ""}>
               {
             [0,1,2,3,4,5,6].map ( (n) => {
                 return (
@@ -99,16 +120,20 @@ class App extends React.Component {
                     weatherIcon={this.weatherIcon(n)} 
                     tempHigh={this.tempHigh(n)} 
                     tempLow={this.tempLow(n)} 
-                    onClick={(e) => this.setState({ isOpenValue: Number(n), isActive: Number(n)})}
-                    className={(n === this.state.isActive ) ? "weatherTiles active" : "weatherTiles"} 
+                    onClick={(e) => this.setState({ isOpenValue: Number(n), isActive: Number(n), menuActive: false})}
+                    className={(n === this.state.isActive ) ? "weatherTiles active" : "weatherTiles"}
                   />
                 )
             })
         }         
             </section>
+            {/* Api link included based off terms of aggrement */}
             <footer>
               <a href="https://darksky.net/poweredby/" target="_blank" rel="noopener noreferrer">Application data provided by Dark Sky</a>
             </footer>
+
+            {/* Mobile Menu control*/}
+            <div className="mobilemenu" onClick={ () => this.menuUpdate() } > Day Menu</div>
             </React.Fragment>
           )}
         </div>
