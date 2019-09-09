5fb0951526bbaf2ad800b8dc7e7763e79cb04d93
Andy Durette <duretteandy@gmail.com>
2019-09-09 15:40:03 -0400

    Removed redundant role

--- src/WeatherTiles.js
+++ src/WeatherTiles.js
@@ -4,7 +4,7 @@ export default class WeatherTiles extends React.Component {
 
 render() {
   return (
-      <button role="button" className={this.props.className} onClick={this.props.onClick}>
+      <button className={this.props.className} onClick={this.props.onClick}>
         <p>{this.props.day}<span className="screen-reader-only">{this.props.datesetEnd}</span></p>
         <p>{this.props.weatherIcon}</p>
         <p><span className="screen-reader-only">High of </span>{this.props.tempHigh}° | <span className="screen-reader-only">Low of </span>{this.props.tempLow}°</p>
